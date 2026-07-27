29022165b6a5028f2ee06fd8b387d520afa867c8
Abhi_Kadam <109713732+Abhishek08337@users.noreply.github.com>
2026-07-27 19:45:31 +0530

    1

---
+++ ec2.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+    region = "ap-south-1"
+    access_key = ""
+    secret_key = ""
+}
+
+resources "aws instance" "webserver" {
+    ami = "ami-01a00762f46d584a1"
+    instance_type = "t3.micro"
+    vpc_security_groups_ids = "sg-024292232aaa78a50"
+    key_name = "abhi"
+    tags = {
+        purpose = "webserver"
+    }
+    count = 2
+}
