3cf4d4274d393a57b7a3e2d52a267940aa2c06b1
Abhi_Kadam <109713732+Abhishek08337@users.noreply.github.com>
2026-08-03 18:52:07 +0530

    a

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,19 @@
 provider "aws" {
     region = "ap-south-1"
     access_key = ""
-    secret_key = ""
+    secret__key = ""
 }
 
-resource "aws instance" "webserver" {
+
+
+resource "aws_instance" "webserver" {
     ami = "ami-01a00762f46d584a1"
     instance_type = "t3.micro"
     vpc_security_group_ids = ["sg-024292232aaa78a50"]
     key_name = "abhi"
     tags = {
+        name = "ec2-iaac-demo" 
         purpose = "webserver"
     }
-    count = 2
+    count = 1
 }
