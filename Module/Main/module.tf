cf054f298e83c0051fbb7d1a85f12e2cadd4055d
Abhi_Kadam <109713732+Abhishek08337@users.noreply.github.com>
2026-08-10 12:55:32 +0530

    a

---
+++ Module/Main/module.tf
@@ -0,0 +1,16 @@
+module "ec2"{
+    source = "/home/abhi/tf/Module/Resource/EC2"
+    ami_id = "ami-01a00762f46d584a1"
+    inst_type = "t3.micro"
+    instance_sg = "sg-024292232aaa78a50"
+    key = "abhi"
+    term_protection = false
+    sub_id = module.VPC.subnet_id
+
+}
+
+module "vpc" {
+    source = "/home/abhi/tf/Module/Resource/VPC"
+    cidr = "10.0.0.0/16"
+    subnet = "10.0.0.0/24"    
+}
--- Module/Resource/EC2/variable.tf
+++ Module/Resource/EC2/variable.tf
@@ -1,21 +1,21 @@
 variable "ami_id"{
-    default = "ami-01a00762f46d584a1"
+
 }
 
 variable "inst_type" {
-    default = "t3.micro"
+  
 }
 
 variable "instance_sg" {
-    default = "sg-024292232aaa78a50"
+  
 }
 
 variable "key" {
-    default = "abhi"
+
 }
 
 variable "term_protection" {
-    default = false
+
 }
 
 variable "sub_id" {
