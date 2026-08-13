915168afae49131556c27f536180b8e7d67573d7
Abhi_Kadam <109713732+Abhishek08337@users.noreply.github.com>
2026-08-13 20:13:37 +0530

    aq

---
+++ provisioners/ec2.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+    region = "ap-south-1"
+    profile = "configs"
+}
+
+resource "aws_instance" "demo_ec2" {
+    ami = var.ami
+    instance_type = var.instance
+    key_name = var.key
+    vpc_security_group_ids = [var.sg]
+    tags = {
+        Name = "demo"
+        purpose = "ec2-demo"
+    }
+
+
+    provisioner "file" {
+        source = "sample.txt"
+        destination = "/home/ec2-user/aws/"
+    }
+
+    provisioner "local-exec" {
+        command = "echo ${self.private_ip} >> private_ip.txt"
+    }
+    connection {
+        type = "ssh"
+        user = "ec2-user"
+        private_key = file("${path.module}/tf.pem")
+        host = "self.public_ip"
+    }
+    provisioner "remote-exec" {
+        inline = [
+            "sudo yum install httpd -y"
+            "sudo systemctl start httpd"
+            "sudo yum update all -y"
+            "sudo yum upgrade"
+            "sudo systemctl enable httpd"
+        ]
+    }
+
+
+}
---
+++ provisioners/variable.tf
@@ -0,0 +1,13 @@
+variable "ami" {
+    default = "ami-035827357e3c7e810"
+}
+variable "instance" {
+    default = "t3.micro"
+}
+variable "sg" {
+    default = "sg-0e26e829ff0d4f034"
+}
+
+variable "key" {
+    default = "free"
+}
