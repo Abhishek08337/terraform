5cdac5fd5d151c2053e71956f93957a2c41e10d5
Abhi_Kadam <109713732+Abhishek08337@users.noreply.github.com>
2026-08-13 18:51:15 +0530

    a

--- varstf/demo.tfvars
+++ varstf/demo.tfvars
@@ -1,4 +1,9 @@
 ami = "ami-017bc606d6a02cb3a"
 sg = "sg-0f5e61b7e0217418b"
-inst_type = "t3.small"
+instance = "t3.small"
 key = "abhi"
+
+
+# terraform apply -var-file="filename.tfvars"
+# use this command to run the tfvars file 
+# the tfvars will work before instead of variable file if run the above command. 
--- varstf/ec2.tf
+++ varstf/ec2.tf
@@ -6,8 +6,8 @@ provider "aws" {
 resource "aws_instance" "demo_1"{
     ami_id = var.ami
     instance_type = var.inst_type
-    vpc_security_group_ids = ["var.sg"]
-    key_name = "var.key"
+    vpc_security_group_ids = [var.sg]
+    key_name = var.key
     tags = {
         Name = "tfvars-demo"
         purpose = "demo"
